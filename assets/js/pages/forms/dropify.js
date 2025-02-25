edb9811aa5e3e96dc5dd71a57643394533f45374
Mohm'd Zayeem <87979884+mohdzayeem@users.noreply.github.com>
2025-02-25 13:54:35 +0530

    Initial commit

---
+++ assets/js/pages/forms/dropify.js
@@ -0,0 +1,31 @@
+$(function() {
+    "use strict";
+    $('.dropify').dropify();
+
+    var drEvent = $('#dropify-event').dropify();
+    drEvent.on('dropify.beforeClear', function(event, element) {
+        return confirm("Do you really want to delete \"" + element.file.name + "\" ?");
+    });
+
+    drEvent.on('dropify.afterClear', function(event, element) {
+        alert('File deleted');
+    });
+	
+	var drEvent1 = $('#dropify-event1').dropify();
+    drEvent1.on('dropify.beforeClear', function(event, element) {
+        return confirm("Do you really want to delete \"" + element.file.name + "\" ?");
+    });
+
+    drEvent1.on('dropify.afterClear', function(event, element) {
+        alert('File deleted');
+    });
+
+    $('.dropify-fr').dropify({
+        messages: {
+            default: 'Glissez-déposez un fichier ici ou cliquez',
+            replace: 'Glissez-déposez un fichier ou cliquez pour remplacer',
+            remove: 'Supprimer',
+            error: 'Désolé, le fichier trop volumineux'
+        }
+    });
+});
